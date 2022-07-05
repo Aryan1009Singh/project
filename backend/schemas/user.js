c8333edc727b35819d165dcf0d1cfe378fb0182a
Aryan <aryan.singh@iitg.ac.in>
2022-07-05 16:44:08 +0530

    save

---
+++ backend/schemas/user.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    name: String,
+    roll: Number,
+    email: String,
+    contact: Number
+});
+
+modules.export = mongoose=;
